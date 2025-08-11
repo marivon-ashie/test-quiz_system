65a95c2b6f6a43d1f7cd3c2a1facd6fe8dea10cf
Lewiston <m11lewiston@gmail.com>
2025-08-11 06:34:36 +0100

    Created added main menu options, created quiz.c and quiz.h

---
+++ program_files/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "quiz.h"
+
+int choice;
+
+
+int main() {
+
+    // Welcome messaging
+    printf("\nQuiz/Test System");
+
+    // Program Menu
+    do {
+
+        printf("\nPlease pick an option from the list below.\n");
+        printf("\n1. Start Quiz");
+        printf("\n2. ChooseQuizMode");
+        printf("\n3. View Past Scores");
+        printf("\n4. Exit Program\n\n");
+        
+        // End of program menu
+
+        scanf("%d", &choice); // save user input as option
+
+        // Analyze user input and respond accordingly
+        switch (choice) {
+
+            case 1:
+
+                startQuiz(); // Load startQuiz function
+                break;
+
+            case 2:
+
+                chooseQuizMode(); // Load chooseQuizMode function
+                break;
+
+            case 3:
+
+                viewPastScores(); // Load viewPastScores function
+                break;
+
+            case 4:
+
+                exitProgram(); // Load exitProgram function
+                break;
+
+            default:
+                printf("\nInvalid input.\n");
+
+        }
+
+    } while (choice != 0); // Ensures program keeps running unless use decides to exit program
+
+    return 0;
+
+}
